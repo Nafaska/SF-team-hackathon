5370a33acc0f042fc2b30f3714ce6e6342b1e88c
Nafaska <viktoria.kovalenko7@gmail.com>
2019-03-22 19:00:16 +0200

    Victoria

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,6 +2,6 @@ package main.java.com;
 
 public class Main {
     public static void main (String... args) {
-        System.out.println("Hello team!");
+        System.out.println("Hello team! Victoria branch");
     }
 }
